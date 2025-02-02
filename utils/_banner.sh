5a3027d0a8b47e747f7e23eb356f53c4354eea54
Cauã Sombra <cauansombra@hotmail.com>
2025-02-02 16:42:58 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -24,14 +24,12 @@ print_banner() {
 printf "${CYAN_LIGHT}";
 
 
-printf ${CYAN_LIGHT}" ____   ____  ________  ____    ____   ________    _       ________  ________  _______   \n";  
-printf ${CYAN_LIGHT}"|_  _| |_  _||_   __  ||_   \  /   _| |_   __  |  / \     |  __   _||_   __  ||_   __ \  \n"; 
-printf ${CYAN_LIGHT}"  \ \   / /    | |_ \_|  |   \/   |     | |_ \_| / _ \    |_/  / /    | |_ \_|  | |__) | \n";
-printf ${CYAN_LIGHT}"   \ \ / /     |  _| _   | |\  /| |     |  _|   / ___ \      .'.' _   |  _| _   |  __ /  \n";
-printf ${CYAN_LIGHT}"    \ ' /     _| |__/ | _| |_\/_| |_   _| |_  _/ /   \ \_  _/ /__/ | _| |__/ | _| |  \ \_\n";
-printf ${CYAN_LIGHT}"     \_/     |________||_____||_____| |_____||____| |____||________||________||____| |___|\n";
-                                                                                                                                                         
-  printf "            \033[1;33m        © CANAL VEM FAZER - https://www.youtube.com/channel/UCwFO9ylM7gHxYIXfJqAo1vQ\n";
+printf ${CYAN_LIGHT}" __  __   ____ ___     ____ _   _    _    _____ \n"
+printf ${CYAN_LIGHT}"|  \/  | / ___|_ _|   / ___| | | |  / \  |_   _|\n"
+printf ${CYAN_LIGHT}"| |\/| || |    | |   | |   | |_| | / _ \   | |  \n"
+printf ${CYAN_LIGHT}"| |  | || |___ | |   | |___|  _  |/ ___ \  | |  \n"
+printf ${CYAN_LIGHT}"|_|  |_| \____|___|   \____|_| |_/_/   \_\ |_|  \n"
+                                       
   printf "${NC}";
 
   printf "\n"
